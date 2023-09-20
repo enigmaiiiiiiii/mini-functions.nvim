acb73c477a9f4f49c966251f6e252accd072fc0a
enigmaiiiiii <enigmaiiiiii@localhost.localdomain>
2023-09-20 15:56:11 +0800

    update

--- lua/mini-functions/init.lua
+++ lua/mini-functions/init.lua
@@ -1,7 +1,6 @@
-
 local replace_with_clipboard = require('mini-functions.mini-replace-with-clipboard').replace_with_clipboard
-local insert_markdown_TOC = require('mini-functions.mini-mdTOC').insert_markdown_TOC
-local get_buffer_path = require('mini-functions.mini-bufferpath').get_buffer_path
+local insert_markdown_TOC = require('mini-functions.mini-insert-markdown-TOC').insert_markdown_TOC
+local get_buffer_path = require('mini-functions.mini-get-buffer-path').get_buffer_path
 
 local M = {}
 
@@ -9,8 +8,7 @@ M.insert_markdown_TOC =  insert_markdown_TOC
 M.replace_with_clipboard= replace_with_clipboard
 M.get_buffer_path = get_buffer_path
 
-vim.api.nvim_set_keymap("n", "cp", ":lua replace_with_clipboard()<CR>", {noremap = true, silent = true})
--- vim.api.nvim_set_keymap("n", "<leader>mt", ":lua InsertMarkdownTOC()<CR>", {noremap = true, silent = true})
+vim.keymap.set('n', 'cp', M.replace_with_clipboard, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>mt', M.insert_markdown_TOC, { noremap = true, silent = true })
 
 return M
